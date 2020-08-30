40acf063056eb9238cfe5074c81693394a7180ed
Eman <eman.cse2008@gmail.com>
2020-08-30 18:46:03 +0200

    add security

---
+++ src/main/java/com/apress/todo/config/ToDoSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.apress.todo.config;
+
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+public class ToDoSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .passwordEncoder(passwordEncoder())
+                .withUser("eman")
+                .password(passwordEncoder().encode("eman"))
+                .roles("ADMIN","USER");
+    }
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .anyRequest().fullyAuthenticated()
+//                .and()
+//                .httpBasic();
+//    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                .permitAll()
+                .anyRequest().fullyAuthenticated()
+                .and()
+                .formLogin().loginPage("/login").permitAll()
+                .and()
+                .logout()
+                .logoutRequestMatcher(
+                        new AntPathRequestMatcher("/logout"))
+                .logoutSuccessUrl("/login")
+                //allows clients (like cURL) to use the basic authentication mechanisms.
+                .and()
+                .httpBasic();
+    }
+}
---
+++ src/main/java/com/apress/todo/domain/ToDo.java
@@ -0,0 +1,46 @@
+package com.apress.todo.domain;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Entity
+@Data
+public class ToDo {
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    private String id;
+    @NotNull
+    @NotBlank
+    private String description;
+    private boolean completed;
+
+    @Column(insertable = true, updatable = false)
+    private LocalDateTime created;
+    private LocalDateTime modified;
+
+    public ToDo(){}
+    public ToDo(String description){
+        this.description = description;
+    }
+
+    @PrePersist
+    void onCreate() {
+        this.setCreated(LocalDateTime.now());
+        this.setModified(LocalDateTime.now());
+    }
+    @PreUpdate
+    void onUpdate() {
+        this.setModified(LocalDateTime.now());
+    }
+}
